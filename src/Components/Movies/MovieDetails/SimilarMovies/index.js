70e05a48af909b1d9182ce218fdc1ec797b00d6e
Leandro <leveditorfc@gmail.com>
2022-05-18 19:11:59 -0300

    fix: adjusting responsive SimilarMovies

--- src/Components/Movies/MovieDetails/SimilarMovies/index.js
+++ src/Components/Movies/MovieDetails/SimilarMovies/index.js
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 
 export default function Similar() {
   const [open, setOpen] = useState(false);
-  const [similiar, setSimiliar] = useState([]);
+  const [similarMovies, setSimiliar] = useState([]);
   const { id } = useParams();
   const cancelButtonRef = useRef(null);
 
@@ -25,52 +25,28 @@ export default function Similar() {
   
   return (
     <>
-      <button
-        className="ml-5 mt-5 pl-5 pr-5 bg-red-900 text-white"
-        onClick={() => setOpen(true)}
-      >
-        Titulos semelhamentes
+      <button className="ml-5 mt-5 pl-5 pr-5 bg-red-900 text-white" onClick={() => setOpen(true)} >
+        Similar Movies
       </button>
 
       <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          initialFocus={cancelButtonRef}
-          onClose={setOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0"
+            enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           <div className="fixed z-10 inset-0 overflow-y-auto">
             <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
+              <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                 <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 ">
                   <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div className="flex justify-between">
                       <div>
-                        <Dialog.Title
-                          as="h3"
-                          className="text-lg leading-6 font-medium text-gray-900"
-                        >
-                          Titulos semelhamentes
+                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                          Similar Movies
                         </Dialog.Title>
                       </div>
                       <div>
@@ -79,17 +55,13 @@ export default function Similar() {
                     </div>
                     <div className="sm:flex sm:items-start">
                       <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                        <div className="grid grid-cols-4">
-                          {similiar.results
-                            ? similiar.results.map((movie) => (
-                                <div className="w-36 pl-5 pt-5">
+                        <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2">
+                          {similarMovies.results ? similarMovies.results.map((movie) => (
+                                <div className="w-36 pl-5 pt-5" key={movie.id}>
                                   <Link to={`/details/${movie.id}`} onClick={() => setOpen(false)}>
                                     <div>
-                                      <img
-                                        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                                        className="rounded-lg"
-                                        alt={movie.title}
-                                      />
+                                      <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                                        className="rounded-lg" alt={movie.title}/>
                                     </div>
                                   </Link>
                                 </div>
