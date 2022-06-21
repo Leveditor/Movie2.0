440603b11167c1b8507c66d8c8a5f3bee7e55441
Leandro <leveditorfc@gmail.com>
2022-06-21 18:26:51 -0300

    fix: changing rating movie

--- src/Components/Serie/TopRatedSerieHome/index.js
+++ src/Components/Serie/TopRatedSerieHome/index.js
@@ -40,26 +40,23 @@ export default function TopRatedSerie() {
         </div>
 
         <SliderButton {...settings}>
-          {firstPage.map((movie) => {
+          {firstPage.map((series) => {
             return (
-              <div className="p-1" key={movie.id}>
+              <div className="p-1" key={series.id}>
                 <div>
-                <Link to={`series-details/${movie.id}`}>
+                <Link to={`series-details/${series.id}`}>
                   <div>
-                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} className="rounded-lg" alt={movie.title} />
+                    <img src={`https://image.tmdb.org/t/p/original/${series.poster_path}`} className="rounded-lg" alt={series.title} />
                   </div>
                 </Link>
                 </div>
                 <div className="flex justify-between">
                   <div>
-                    <small className="pl-1 text-gray-400">
-                      {movie.first_air_date}
-                    </small>
+                    <small className="pl-1 text-gray-400">{series.first_air_date}</small>
                   </div>
-                  <div>
-                    <p className={movie.vote_average.toString().replace(".", "") > 70 ? 'text-green-400' : 'text-yellow-500'}>
-                      {movie.vote_average.toString().replace(".", "")}%
-                    </p>
+                  <div className='flex items-center text-yellow-500 pr-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"> <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /></svg>
+                    <p>{series.vote_average}</p>
                   </div>
                 </div>
               </div>
@@ -70,29 +67,23 @@ export default function TopRatedSerie() {
 
       <div className="p-8 pt-0">
         <SliderButton {...settings}>
-          {secondPage.map((movie) => {
+          {secondPage.map((series) => {
             return (
-              <div className="p-1" key={movie.id}>
+              <div className="p-1" key={series.id}>
                 <div>
-                <Link to={`series-details/${movie.id}`}>
+                <Link to={`series-details/${series.id}`}>
                   <div>
-                    <img
-                      src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                      className="rounded-lg"
-                      alt={movie.title} />
+                    <img src={`https://image.tmdb.org/t/p/original/${series.poster_path}`} className="rounded-lg" alt={series.title} />
                   </div>
                 </Link>
                 </div>
                 <div className="flex justify-between">
                   <div>
-                    <small className="pl-1 text-gray-400">
-                      {movie.first_air_date}
-                    </small>
+                    <small className="pl-1 text-gray-400">{series.first_air_date}</small>
                   </div>
-                  <div>
-                    <p className="text-green-400 pr-4">
-                      {movie.vote_average.toString().replace(".", "")}%
-                    </p>
+                  <div className='flex items-center text-yellow-500 pr-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"> <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /></svg>
+                    <p>{series.vote_average}</p>
                   </div>
                 </div>
               </div>
